48a30d09d186331d115d05cbb61aec3245c5170f
Claudio Diener <diener.claudio@gmail.com>
2021-12-14 13:03:31 +0100

    Create card for places

---
+++ Nappoli/CardPlaces.swift
@@ -0,0 +1,86 @@
+//
+//  CardPlaces.swift
+//  Nappoli
+//
+//  Created by Claudio Diener on 11/12/21.
+//
+
+import SwiftUI
+
+struct CardPlaces: View {
+    
+    @State var isLiked = false
+    
+    var body: some View {
+        VStack (spacing: 0) {
+            ZStack (alignment: .bottom) {
+                Image("RoyalPalacePlaces")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                HStack {
+                    Text("Royal Palace")
+                        .font(.system(size:25))
+                        .fontWeight(.heavy)
+                        .bold()
+                        .foregroundColor(.white)
+                        .shadow(color: .primary, radius: 5)
+                    Spacer()
+                    Button(action: {
+                        self.isLiked.toggle()
+                    }) {
+                        Image(systemName: isLiked ? "heart.fill" : "heart.circle.fill")
+                            .resizable()
+                            .foregroundColor(.white)
+                            .frame(width: 25, height: 25)
+                            .shadow(color: .primary, radius: 5)
+                    }
+                }
+                .padding()
+            }
+            ZStack {
+                Color.white
+                VStack {
+                    HStack {
+                        Image(systemName: "calendar")
+                            .foregroundColor(.black)
+                        Text("Mon Tue Thu Fri Sat Sun")
+                            .font(.system(size:11))
+                            .bold()
+                            .foregroundColor(.black)
+                        Spacer()
+                        Image(systemName: "clock.fill")
+                            .foregroundColor(.black)
+                        Text("9 AM - 8 PM")
+                            .font(.system(size:11))
+                            .bold()
+                            .foregroundColor(.black)
+                    }
+                    .padding()
+                    HStack {
+                        Text("€ 6,00")
+                            .font(.system(size: 25))
+                            .bold()
+                            .foregroundColor(.green)
+                        Spacer()
+                        Text("More")
+                            .font(.system(size: 25))
+                            .bold()
+                            .foregroundColor(.black)
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.black)
+                    }
+                    .padding()
+                }
+            }
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
+            .shadow(radius: 5)
+            .frame(width: 360, height: 200)
+    }
+}
+
+struct CardPlaces_Previews: PreviewProvider {
+    static var previews: some View {
+        CardPlaces()
+    }
+}
