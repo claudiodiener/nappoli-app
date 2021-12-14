05c7cdaed1dbbe7b900b09fab3eb0180886d0bf4
Claudio Diener <diener.claudio@gmail.com>
2021-12-14 13:04:05 +0100

    Create searchbar

---
+++ Nappoli/ViewControllerResolver.swift
@@ -0,0 +1,40 @@
+//
+//  ViewControllerResolver.swift
+//  Nappoli
+//
+//  Created by Claudio Diener on 12/12/21.
+//
+
+import SwiftUI
+
+final class ViewControllerResolver: UIViewControllerRepresentable {
+    let onResolve: (UIViewController) -> Void
+    
+    init(onResolve: @escaping(UIViewController) -> Void) {
+        self.onResolve = onResolve
+    }
+    func makeUIViewController(context: Context) -> ParentResolverViewController {
+        ParentResolverViewController(onResolve: onResolve)
+    }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+class ParentResolverViewController: UIViewController {
+    let onResolve: (UIViewController) -> Void
+    
+    init(onResolve: @escaping (UIViewController) -> Void) {
+    self.onResolve = onResolve
+        super.init(nibName: nil, bundle: nil)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("Use init(onResolve) to intantiate ParentResolverViewController.")
+    }
+    override func didMove(toParent parent: UIViewController?) {
+        super.didMove(toParent: parent)
+        if let parent = parent {
+            onResolve(parent)
+        }
+    }
+}
