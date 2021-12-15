548f1a36fca73776405f80b702ac4448c3ac25e0
Claudio Diener <diener.claudio@gmail.com>
2021-12-15 17:27:56 +0100

    Add pin on map

--- Nappoli/MapLocation.swift
+++ Nappoli/MapLocation.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 import MapKit
 
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct MapLocation: View {
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 40.836_167, longitude: 14.249_557),
         span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
     )
+    let annotation = [
+        Location(name: "Royal Palace", coordinate: CLLocationCoordinate2D(
+        latitude: 40.836_167, longitude: 14.249_557))
+    ]
+ 
     var body: some View {
-        Map(coordinateRegion: $region)
-           .ignoresSafeArea(edges: .top)
+        Map(coordinateRegion: $region, annotationItems: annotation) { Location in
+            MapAnnotation(coordinate: Location.coordinate) {
+                    Image("Position")
+                    Text("Royal Palace")
+                    .fixedSize()
+                    .shadow(color: .secondary, radius: 5)
+            }
+        }
+        .ignoresSafeArea(edges: .top)
     }
 }
 
